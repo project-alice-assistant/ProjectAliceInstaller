b35a0aeb9177798a8653c4ca97d4f25658111201
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-06-12 15:51:16 +0200

    booooooring

--- installer.sh
+++ installer.sh
@@ -142,25 +142,25 @@ case "$choice" in
 				select region in "East USA (Ohio)" "East USA (North Virginia)" "West USA (North California)" "West USA (Oregon)" "Asia Pacific (Hong Kong)" "Asia Pacific (Mumbai)" "Asia Pacific (Seoul)" "Asia Pacific (Singapour)" "Asia Pacific (Sydney)" "Asia Pacific (Tokyo)" "Canada (center)" "China (Beijing)" "China (Ningxia)" "EU (Francfort)" "EU (Irlande)" "EU (London)" "EU (Paris)" "EU (Stockholm)" "South America (Sao Paulo)";
 				do
 					case "$region" in
-						"1" ) awsAPIGateway="us-east-2";;
-						"2" ) awsAPIGateway="us-east-1";;
-						"3" ) awsAPIGateway="us-west-1";;
-						"4" ) awsAPIGateway="us-west-2";;
-						"5" ) awsAPIGateway="ap-east-1";;
-						"6" ) awsAPIGateway="ap-south-1";;
-						"7" ) awsAPIGateway="ap-northeast-2";;
-						"8" ) awsAPIGateway="ap-southeast-1";;
-						"9" ) awsAPIGateway="ap-southeast-2";;
-						"10" ) awsAPIGateway="ap-northeast-1";;
-						"11" ) awsAPIGateway="ca-central-1";;
-						"12" ) awsAPIGateway="cn-north-1";;
-						"13" ) awsAPIGateway="cn-northwest-1";;
-						"14" ) awsAPIGateway="eu-central-1";;
-						"15" ) awsAPIGateway="eu-west-1";;
-						"16" ) awsAPIGateway="eu-west-2";;
-						"17" ) awsAPIGateway="eu-west-3";;
-						"18" ) awsAPIGateway="eu-north-1";;
-						"19" ) awsAPIGateway="sa-east-1";;
+						"East USA (Ohio)") awsAPIGateway="us-east-2";;
+						"East USA (North Virginia)") awsAPIGateway="us-east-1";;
+						"West USA (North California)") awsAPIGateway="us-west-1";;
+						"West USA (Oregon)") awsAPIGateway="us-west-2";;
+						"Asia Pacific (Hong Kong)") awsAPIGateway="ap-east-1";;
+						"Asia Pacific (Mumbai)") awsAPIGateway="ap-south-1";;
+						"Asia Pacific (Seoul)") awsAPIGateway="ap-northeast-2";;
+						"Asia Pacific (Singapour)") awsAPIGateway="ap-southeast-1";;
+						"Asia Pacific (Sydney)") awsAPIGateway="ap-southeast-2";;
+						"Asia Pacific (Tokyo)") awsAPIGateway="ap-northeast-1";;
+						"Canada (center)") awsAPIGateway="ca-central-1";;
+						"China (Beijing)") awsAPIGateway="cn-north-1";;
+						"China (Ningxia)") awsAPIGateway="cn-northwest-1";;
+						"EU (Francfort)") awsAPIGateway="eu-central-1";;
+						"EU (Irlande)") awsAPIGateway="eu-west-1";;
+						"EU (London)") awsAPIGateway="eu-west-2";;
+						"EU (Paris)") awsAPIGateway="eu-west-3";;
+						"EU (Stockholm)") awsAPIGateway="eu-north-1";;
+						"South America (Sao Paulo)") awsAPIGateway="sa-east-1";;
 						*) echo -e "\e[32mInvalid choice\e[0m";;
 					esac
 				done
