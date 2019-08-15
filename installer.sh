2ec3cc124174eb7aecf8bb3de7153cb590a5436c
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-08-15 11:23:03 +0200

    Looks like it's still needed

--- installer.sh
+++ installer.sh
@@ -730,15 +730,15 @@ case "$choice" in
 		fi
 
 		if [[ "$ttsService" == "google" || "$ttsService" == "both" ]]; then
-			#cd ${USERDIR}
-			#export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-			#echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-			#curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-			#apt-get update && sudo apt-get install -y google-cloud-sdk
-			#gcloud init
+			cd ${USERDIR}
+			export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+			echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+			curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+			apt-get update && sudo apt-get install -y google-cloud-sdk
+			gcloud init
 
-			#mkdir -p /var/empty/.config/gcloud
-			#chown _snips /var/empty/.config/gcloud
+			mkdir -p /var/empty/.config/gcloud
+			chown _snips /var/empty/.config/gcloud
 
 			pip3 install --upgrade google-cloud-texttospeech
 		fi
